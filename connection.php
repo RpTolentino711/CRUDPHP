779e4440fcbc452d44f4e857ae61c32af491401f
itscool <romeopaolotolentino@gmail.com>
2025-05-19 22:52:09 +0800

    Initial push

--- connection.php
+++ connection.php
@@ -3,6 +3,6 @@ $connection = mysqli_connect("localhost","root","","mydb");
 if(mysqli_connect_errno()){
     echo "Failed to connect to MySQL:" . mysqli_connect_error();
 }else{
-    echo "connected";
+    echo "";
 }
 ?>
